13ce11e05690e85c2d3bef73c17354d331ecccc8
Stephan Bösch-Plepelits <plepelits@cg.tuwien.ac.at>
2017-05-09 10:16:31 +0200

    shadow_to_unixtime/shadow_from_unixtime(): new functions

--- functions.php
+++ functions.php
@@ -13,3 +13,21 @@ function remove_ldap_count(&$data) {
     remove_ldap_count($data[$k]);
   }
 }
+
+/**
+ * convert a shadow timestamp (days since epoch) to a unix timestamp
+ */
+function shadow_to_unixtime($t) {
+  return (int)($t * 24 * 60 * 60);
+}
+
+/**
+ * convert a unix timestamp to a shadow timestamp (days since epoch). Default:
+ * today.
+ */
+function shadow_from_unixtime($t=null) {
+  if($t === null)
+    $t = time();
+
+  return (int)($t / 24 / 60 / 60);
+}
